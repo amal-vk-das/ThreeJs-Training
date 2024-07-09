fce8bd3ab35790f3001ee968bcc6cc2e5661b300
amal-vk-das <amal.das@jobinandjismi.com>
2024-07-09 09:53:03 +0530

    changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as THREE from './node_modules/three/src/Three.js'
-import { OrbitControls } from './src/OrbitControls.js';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import getStarfield from "./src/getStarfield.js";
 import { getFresnelMat } from "./src/getFresnelMat.js";
 
